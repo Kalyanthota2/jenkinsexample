485003c2157fa63ff39a2020bdbe0b48320cc9e6
kalyanmac <kalyan.thota2@gmail.com>
2022-08-17 20:25:22 +0530

    first commit

---
+++ src/main/java/com/jenkins/jenkinexample/JenkinexampleApplication.java
@@ -0,0 +1,16 @@
+package com.jenkins.jenkinexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinexampleApplication.class, args);
+
+		System.out.println("****** Hello World *********");
+	}
+
+
+}
---
+++ src/test/java/com/jenkins/jenkinexample/JenkinexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
